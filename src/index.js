0b6dadf33d16ca415bce0831cf40fa4360a4f0f0
Ilya Kiryliuk <ilya.ey@mail.ru>
2019-03-09 23:51:50 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var j = 0;
+  var k = 0;
+  var m = 0;
+  var l = 0;
+
+  for(i = 0; i < str.length; i++){
+    if ((str[i] == '(') || (str[i] == '[') || (str[i] == '{') || (str[i] == '1') || 
+    (str[i] == '3') || (str[i] == '5')){
+      j++;
+    }
+    if ((str[i] == ')') || (str[i] == ']') || (str[i] == '}') || (str[i] == '2') || 
+    (str[i] == '4') || (str[i] == '6')){
+      j--;
+    }
+    if(str[i] == '|'){
+      k++;
+    }  
+    if(str[i] == '7'){
+      m++;
+    }  
+    if(str[i] == '8'){
+      l++;
+    }  
+  }
+
+  if ((j == 0) && ((k % 2) == 0) && ((m % 2) == 0) && ((l % 2) == 0)){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
